06811b4aab6664e6ed5727af7b3af16bc3bb4967
sunguangxuan <sunguangxuan@edicdn.eu.org>
2024-04-02 12:27:59 +0800

    添加远程管理

--- wamp/testmysql.php
+++ wamp/testmysql.php
@@ -5,7 +5,7 @@
 *
 */
 $user = 'root';
-$password = ''; //To be completed if you have set a password to root
+$password = '2023ji5ban'; //To be completed if you have set a password to root
 $database = ''; //To be completed to connect to a database. The database must exist.
 $port = NULL; //Default must be NULL to use default port
 $mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
